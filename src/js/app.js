e4510b93bfc41c4fa8f466c05fc2584089c43bd3
Asura Enkhbayar <asura.enkhbayar@gmail.com>
2016-09-15 14:33:25 +0200

    proper gulp setup and added assets

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,8 @@
+/*jslint browser:true */
+"use strict";
+
 $(document).foundation();
 
 $.get( "http://api.icndb.com/jokes/random?limitTo=[nerdy]" , function(data) {
-	$("#chuck_norris").html('"' + data.value.joke + '"');
+	$("#chuck_norris").html("\"" + data.value.joke + "\"");
 });
